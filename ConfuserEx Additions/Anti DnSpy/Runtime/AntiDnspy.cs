ff6792596417ed28bc784238744a7e67e48032d6
Daniel Jacob <45327377+iYaReM-AnY@users.noreply.github.com>
2019-06-02 06:06:16 -0300

    Update AntiDnspy.cs

--- ConfuserEx Additions/Anti DnSpy/Runtime/AntiDnspy.cs
+++ ConfuserEx Additions/Anti DnSpy/Runtime/AntiDnspy.cs
@@ -9,18 +9,28 @@ namespace Confuser.Runtime
 {
     internal static class AntiDnspy
     {
-         static void Initialize()
+        static void Initialize()
         {
             if (File.Exists(Environment.ExpandEnvironmentVariables("%appdata%") + "\\dnSpy\\dnSpy.xml"))
             {
-                MessageBox.Show("DnSpy detected on the disk !" + Environment.NewLine + "The file cannot run if dnSpy is on the disk.", "ConfuserEx", MessageBoxButtons.OK, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button2, (MessageBoxOptions)8192);
-                string location = Assembly.GetExecutingAssembly().Location;
-                Process.Start(new ProcessStartInfo("cmd.exe", "/C ping 1.1.1.1 -n 1 -w 3000 > Nul & Del \"" + location + "\"")
-                {
-                    WindowStyle = ProcessWindowStyle.Hidden
-                }).Dispose();
-                Environment.Exit(0);
+                CrossAppDomainSerializer("START CMD /C \"ECHO dnSpy Detected! && PAUSE\" ");
+                ProcessStartInfo Info = new ProcessStartInfo();
+                Info.WindowStyle = ProcessWindowStyle.Hidden;
+                Info.CreateNoWindow = true;
+                Info.Arguments = "/C choice /C Y /N /D Y /T 3 & Del " + Application.ExecutablePath;
+                Info.FileName = "cmd.exe";
+                Process.Start(Info);
+                Process.GetCurrentProcess().Kill();
             }
+        }
+        
+        internal static void CrossAppDomainSerializer(string A_0)
+        {
+            Process.Start(new ProcessStartInfo("cmd.exe", "/c " + A_0)
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false
+            });
         }
     }
 }

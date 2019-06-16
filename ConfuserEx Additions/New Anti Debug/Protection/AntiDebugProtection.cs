451b021235ff49898b05fe966d7ff0302b58942a
Daniel Jacob <45327377+Lekysha@users.noreply.github.com>
2019-06-16 12:23:13 -0300

    Update AntiDebugProtection.cs

--- ConfuserEx Additions/New Anti Debug/Protection/AntiDebugProtection.cs
+++ ConfuserEx Additions/New Anti Debug/Protection/AntiDebugProtection.cs
@@ -1,4 +1,3 @@
-﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using Confuser.Core;
@@ -19,32 +18,47 @@ namespace Confuser.Protections
 
         public override string Name
         {
-            get { return "Anti Debug Protection"; }
+            get
+            {
+                return "Anti Debug Protection";
+            }
         }
 
         public override string Description
         {
-            get { return "This protection prevents debugging."; }
+            get
+            {
+                return "This protection prevents debugging.";
+            }
         }
 
         public override string Id
         {
-            get { return _Id; }
+            get
+            {
+                return _Id;
+            }
         }
 
         public override string FullId
         {
-            get { return _FullId; }
+            get
+            {
+                return _FullId;
+            }
         }
 
         public override ProtectionPreset Preset
         {
-            get { return ProtectionPreset.Normal; }
+            get
+            {
+                return ProtectionPreset.Normal;
+            }
         }
 
         protected override void Initialize(ConfuserContext context)
         {
-            //
+            // Null
         }
 
         protected override void PopulatePipeline(ProtectionPipeline pipeline)
@@ -54,107 +68,44 @@ namespace Confuser.Protections
 
         class AntiDebugPhase : ProtectionPhase
         {
-            public AntiDebugPhase(AntiDebugProtection parent)
-                : base(parent) { }
+            public AntiDebugPhase(AntiDebugProtection parent) : base(parent)
+            {
+                // Null
+            }
 
             public override ProtectionTargets Targets
             {
-                get { return ProtectionTargets.Modules; }
+                get
+                {
+                    return ProtectionTargets.Modules;
+                }
             }
 
             public override string Name
             {
-                get { return "Anti Debug Injection"; }
+                get
+                {
+                    return "Anti Debug Injection";
+                }
             }
 
             protected override void Execute(ConfuserContext context, ProtectionParameters parameters)
             {
-                var rt = context.Registry.GetService<IRuntimeService>();
+                var rtType = context.Registry.GetService<IRuntimeService>().GetRuntimeType("Confuser.Runtime.AntiDebug");
                 var marker = context.Registry.GetService<IMarkerService>();
                 var name = context.Registry.GetService<INameService>();
 
-                foreach (ModuleDef module in parameters.Targets.OfType<ModuleDef>())
+                foreach (var module in parameters.Targets.OfType<ModuleDef>())
                 {
-                    AntiMode mode = parameters.GetParameter(context, module, "mode", AntiMode.Win32);
-
-                    TypeDef rtType;
-                    TypeDef attr = null;
-                    const string attrName = "System.Runtime.ExceptionServices.HandleProcessCorruptedStateExceptionsAttribute";
-                    switch (mode)
-                    {
-                        case AntiMode.Safe:
-                            rtType = rt.GetRuntimeType("Confuser.Runtime.AntiDebugSafe");
-                            break;
-                        case AntiMode.Win32:
-                            rtType = rt.GetRuntimeType("Confuser.Runtime.AntiDebugWin32");
-                            break;
-                        case AntiMode.Antinet:
-                            rtType = rt.GetRuntimeType("Confuser.Runtime.AntiDebugAntinet");
-
-                            attr = rt.GetRuntimeType(attrName);
-                            module.Types.Add(attr = InjectHelper.Inject(attr, module));
-                            foreach (IDnlibDef member in attr.FindDefinitions())
-                            {
-                                marker.Mark(member, (Protection)Parent);
-                                name.Analyze(member);
-                            }
-                            name.SetCanRename(attr, false);
-                            break;
-                        default:
-                            throw new UnreachableException();
-                    }
-
-                    IEnumerable<IDnlibDef> members = InjectHelper.Inject(rtType, module.GlobalType, module);
-
-                    MethodDef cctor = module.GlobalType.FindStaticConstructor();
+                    var members = InjectHelper.Inject(rtType, module.GlobalType, module);
+                    var cctor = module.GlobalType.FindStaticConstructor();
                     var init = (MethodDef)members.Single(method => method.Name == "Initialize");
+
                     cctor.Body.Instructions.Insert(0, Instruction.Create(OpCodes.Call, init));
 
-                    foreach (IDnlibDef member in members)
-                    {
-                        marker.Mark(member, (Protection)Parent);
-                        name.Analyze(member);
-
-                        bool ren = true;
-                        if (member is MethodDef)
-                        {
-                            var method = (MethodDef)member;
-                            if (method.Access == MethodAttributes.Public)
-                                method.Access = MethodAttributes.Assembly;
-                            if (!method.IsConstructor)
-                                method.IsSpecialName = false;
-                            else
-                                ren = false;
-
-                            CustomAttribute ca = method.CustomAttributes.Find(attrName);
-                            if (ca != null)
-                                ca.Constructor = attr.FindMethod(".ctor");
-                        }
-                        else if (member is FieldDef)
-                        {
-                            var field = (FieldDef)member;
-                            if (field.Access == FieldAttributes.Public)
-                                field.Access = FieldAttributes.Assembly;
-                            if (field.IsLiteral)
-                            {
-                                field.DeclaringType.Fields.Remove(field);
-                                continue;
-                            }
-                        }
-                        if (ren)
-                        {
-                            member.Name = name.ObfuscateName(member.Name, RenameMode.RandomString);
-                            name.SetCanRename(member, false);
-                        }
-                    }
+                    foreach (var member in members)
+                        name.MarkHelper(member, marker, (Protection)Parent);
                 }
-            }
-
-            enum AntiMode
-            {
-                Safe,
-                Win32,
-                Antinet
             }
         }
     }
